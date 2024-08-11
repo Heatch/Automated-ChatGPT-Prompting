d054ca262a418c2293cc7042b0d7a2b49789f2ad
Hitesh <hiteshguptacad@gmail.com>
2024-08-10 21:11:01 -0400

    init

---
+++ index.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+from openpyxl import load_workbook
+
+# RUN BAT FILE FIRST TO START CHROME WITH REMOTE DEBUGGING ENABLED
+
+# Specify the port you've used for remote debugging
+debugging_port = 9222
+sheet = "./test-file.xlsx"
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:{debugging_port}")
+
+# Path to your ChromeDriver executable
+chrome_driver_path = "./chromedriver.exe"
+
+# Set up ChromeDriver with the options
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# Find the textarea and submit button
+textarea = driver.find_element(By.XPATH, '//*[@id="prompt-textarea"]')
+submit_button = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/main/div[1]/div[2]/div[1]/div/form/div/div[2]/div/div/button')
+
+# Read the prompts from the Excel file
+df = pd.read_excel(sheet, engine='openpyxl', dtype=object, header=None)
+cellTexts = df.values.tolist()
+cellTexts = cellTexts[1:]
+
+# Generate responses for each prompt
+startingIndex = 3
+generationStatus = False
+responses = []
+for prompt in cellTexts:
+    textarea.send_keys(prompt[0])
+    submit_button.click()
+    while not generationStatus:
+        try:
+            find_test = driver.find_element(By.XPATH, f'//*[@id="__next"]/div/div[2]/main/div[1]/div[1]/div/div/div/div/div[{startingIndex}]/div/div/div[2]/div/div[2]/div')
+            generationStatus = True
+        except:
+            time.sleep(1)
+    response = driver.find_element(By.XPATH, f'//*[@id="__next"]/div/div[2]/main/div[1]/div[1]/div/div/div/div/div[{startingIndex}]/div/div/div[2]/div/div[1]/div/div/div')
+    responses.append(response.text)
+    startingIndex += 2
+    generationStatus = False
+
+# Write the responses back to the Excel file
+wb = load_workbook(filename=sheet)
+ws = wb['Sheet1']
+
+for i in range(len(responses)):
+    ws.cell(row=i+2, column=2).value = responses[i]
+
+wb.save(filename=sheet)
---
+++ start-chrome.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\hites\Documents\Selenium ChatGPT\chrome-win64\chrome.exe" --remote-debugging-port=9222
+:: Change the path to the location of your chrome.exe (chrome for developers)
+:: Change the port number if you want to use a different port, ensure it matches the port in the code
